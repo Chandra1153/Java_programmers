75c5e28efa863c6dd27f9eb493eb237f496ded5b
Chandra2k <117076931+Chandra1153@users.noreply.github.com>
2023-11-28 01:10:24 +0900

    [level unrated] Title: 더 크게 합치기, Time: 10.71 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        String str1 = "";
-        String str2 = "";
-
-        str1 = Integer.toString(a) + Integer.toString(b);
-        str2 = Integer.toString(b) + Integer.toString(a);
-        if (Integer.parseInt(str1) > Integer.parseInt(str2))
-            answer = Integer.parseInt(str1);
-        else
-            answer = Integer.parseInt(str2);
+        int ab = Integer.parseInt("" + a + b);
+        int ba = Integer.parseInt("" + b + a);
+        answer = ab > ba ? ab : ba;
         return answer;
+
     }
 }
