260b7de26bc3f894aca6474e9f5a17709196bea2
Chandra2k <117076931+Chandra1153@users.noreply.github.com>
2024-01-23 21:50:44 +0900

    [level 0] Title: 수 조작하기 2, Time: 634.52 ms, Memory: 388 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        int n;
+        for (int i = 1; i < numLog.length; i++) {
+            if (numLog[i] > numLog[i-1])
+                answer = answer + ((numLog[i] - numLog[i-1]) == 1 ? 'w' : 'd');
+            else 
+                answer = answer + ((numLog[i-1] - numLog[i]) == 1 ? 's' : 'a');
+        }
+        return answer;
+    }
+}
